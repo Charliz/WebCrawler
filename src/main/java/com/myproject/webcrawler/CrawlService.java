60f9e476aa047920f56c839214cc057078242907
Charliz <luckycatttt@gmail.com>
2017-09-22 00:48:31 +0400

    added comments

--- src/main/java/com/myproject/webcrawler/CrawlService.java
+++ src/main/java/com/myproject/webcrawler/CrawlService.java
@@ -3,6 +3,7 @@ package com.myproject.webcrawler;
 public class CrawlService {
     public static void main(String[] args) {
 
+        // Start Crawling!
         CrawlController crawlController = new CrawlController();
         crawlController.startCrawling();
     }
